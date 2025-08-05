0510a3592d7f0a00dbf259040bb59d0993c2bd30
NGC-3628 <isaacvgn@gmail.com>
2025-08-05 14:42:48 -0600

    w5

--- week03/learn/obj/Debug/net8.0/learn.AssemblyInfo.cs
+++ week03/learn/obj/Debug/net8.0/learn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29e6603a5ad3b8e56903df8104dda9687372c1d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3349c1f7ff6ccd52546da300b72fb631bb1137fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("learn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
